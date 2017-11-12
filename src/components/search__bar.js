04a382f9f1c421523890f04b35fa2bd099daf86d
Jamie Lockie <jamielockie@gmail.com>
2017-11-12 16:18:24 -0500

    event handler added

--- src/components/search__bar.js
+++ src/components/search__bar.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// functional component (dumb!, Presenational) -- not using ES6 class to allow it to have state
 
-const SearchBar = () => {
-    return <input />
-};
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+// Class Component
+
+class SearchBar extends Component {
+    render() {
+        return <input onChange={event => console.log('event.target.value') } />;
+    }
+
+}
+
 
 export default SearchBar;
