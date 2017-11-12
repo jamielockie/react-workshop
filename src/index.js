f469b467059d01a9ce7e2801ad326243aeb17ea4
Jamie Lockie <jamielockie@gmail.com>
2017-11-12 15:56:05 -0500

    Checking upstream

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search__bar';
 
+// Youtube Search API v4 
 const API_KEY = 'AIzaSyBICv_6WJ76NB9M0Y0V4Jv0A7e4Vz9OHeQ';
 
 // Create a new component. This component should produce some html
