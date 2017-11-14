1cc16deb694af27661759fa622dffb6b0b7a451d
Jamie Lockie <jamielockie@gmail.com>
2017-11-14 09:17:16 -0500

    App goes from simple to complex

--- src/components/search__bar.js
+++ src/components/search__bar.js
@@ -10,10 +10,22 @@ import ReactDOM from 'react-dom';
 // Class Component
 
 class SearchBar extends Component {
-    render() {
-        return <input onChange={event => console.log('event.target.value') } />;
+    constructor(props) {
+        super(props);
+
+        this.state = { term: 'Starting Value' };
     }
 
+    render() {
+        return ( 
+        <div>
+            <input 
+            value={this.state.term}
+
+            onChange={event => this.setState({ term: event.target.value})} />;
+        </div>
+        );
+    }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; 
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search__bar';
 
 // Youtube Search API v4 
 const API_KEY = 'AIzaSyBICv_6WJ76NB9M0Y0V4Jv0A7e4Vz9OHeQ';
 
-// Create a new component. This component should produce some html
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos: videos }); /* Since these are the same, we can just put ({ videos }) */
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated html and put it on the page (In the dom)
