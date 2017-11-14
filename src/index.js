91757b2467c381f79c699d2357980b6a5813a3ea
Jamie Lockie <jamielockie@gmail.com>
2017-11-14 11:24:40 -0500

    video embed fixed

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[1]} />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
